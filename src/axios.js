f8d38eb72c1c29c478b759094d58a57e191e8751
dillingham <brian@dillingham.dev>
2022-07-02 01:07:25 -0400

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,11 @@
-import Axios from 'axios'
+import Axios from "axios";
 
 const axios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,    
+    baseURL: process.env['NEXT_PUBLIC_BACKEND_URL'],
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
     withCredentials: true,
 })
 
-export default axios
+export default axios;
